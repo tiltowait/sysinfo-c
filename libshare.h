bb09e6d00a70a353cd0494cc3989a4d4034d4990
tiltowait <208040+tiltowait@users.noreply.github.com>
2025-11-18 20:23:06 -0800

    Fix warnings

--- libshare.h
+++ libshare.h
@@ -2,11 +2,17 @@
 #ifndef _LIBSHARE_H
 #define _LIBSHARE_H
 
-/* 
- * Defines a dummy type for sa_share_t.
+/*
+ * Defines dummy types for libshare structures.
  * This allows libzfs.h to compile without the real internal header.
  */
 typedef void * sa_share_t;
+
+/* Dummy enum for sa_protocol to silence compiler warnings */
+enum sa_protocol {
+	SA_PROTOCOL_NFS = 0,
+	SA_PROTOCOL_SMB = 1
+};
 
 #endif
 
